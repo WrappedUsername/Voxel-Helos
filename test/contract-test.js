e6d21d413cbf4d11c4856e825d97ee62f821317f
Keefe Overby <you@example.com>
2022-06-27 15:16:54 -0600

    Modern JavaScript - WrappedUsername

--- test/contract-test.js
+++ test/contract-test.js
@@ -7,22 +7,21 @@ import { ethers } from "hardhat";
 import { expect } from "chai";
 
 // new javascript
-describe = () => ("Voxel Helos Genesis Smart Contract Tests",  {
+describe("Voxel Helos Genesis Smart Contract Tests", () =>  {
 
-  this:beforeEach = async () => {
+  beforeEach(() => {
   const VoxelHelosGenesis = await ethers.getContractFactory("Voxel helos Genesis");
   const voxelHelosGenesis = await delpoy(VoxelHelosGenesis);
   await voxelHelosGenesis.deployed();
   console.log("Voxel Helos Genesis deployed to:", voxelHelosGenesis.address);
-  },
+  }),
   // WIP
-  it: async () => ("VHG NFT minted successfully", {
-    [account1]: await ethers.getSigners(),
-    expect: async () => {
-      await voxelHelosGenesis.balanceOf(account1.address).to.equal(0)
-    },
-    const: voxelHelosGenesis = await VoxelHelosGenesis.safeMint(),
-    console:log("Minted:", _tokenIdCounter)
+  it("VHG NFT minted successfully", () =>  {
+    [account1]; await ethers.getSigners();
+    expect; await voxelHelosGenesis.balanceOf(account1.address).to.equal(0);
+    
+    const voxelHelosGenesis = await voxelHelosGenesis.safeMint();
+    console.log("Minted:", _tokenIdCounter)
     
   })
   
